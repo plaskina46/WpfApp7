84da4f3a0091186b9b79596d49eb0b6a785182a4
Ann <Ann@DESKTOP-1NFLL33>
2023-09-14 13:24:59 +0900

    Добавьте файлы проекта.

---
+++ WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = "server=127.0.0.1; port=3307; user=root;password=; database=AutoAll";
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT * FROM users WHERE login = @login AND password=@password";
+                MySqlCommand command = new MySqlCommand(query, connection);
+                command.Parameters.AddWithValue("@login", tb_login.Text);
+                command.Parameters.AddWithValue("@password", pass.Password);
+                int result = Convert.ToInt32(command.ExecuteScalar());
+                if (result > 0)
+                {
+                    MessageBox.Show("зашлии");
+                    Window1 main = new Window1();
+                    main.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("ytdth");
+
+                }
+
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+
+
+}
+
+
+
+
+
+
